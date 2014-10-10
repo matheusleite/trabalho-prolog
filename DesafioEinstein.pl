bc757447f39b61155ff3702f6aaeb96e8476e099
Matheus Leite <leitematheus7@hotmail.com>
2014-10-10 12:34:09 -0300

    Creating facts in a semantics network.

---
+++ DesafioEinstein.pl
@@ -0,0 +1,58 @@
+:-op(900,xfx,[ehUmTipoDe, tem, temUm]).
+
+homem temUm nacionalidade.
+homem temUm casa.
+homem temUm animal.
+homem temUm cigarro.
+homem temUm bebida.
+homem tem vizinhoEsq.
+homem tem vizinhoDir.
+
+alemao ehUmTipoDe homem.
+noruegues ehUmTipoDe homem.
+sueco ehUmTipoDe homem.
+ingles ehUmTipoDe homem.
+dinamarques ehUmTipoDe homem.
+
+alemao tem nacionalidade = alema.
+noruegues tem nacionalidade = noruega.
+sueco tem nacionalidade = sueca.
+ingles tem nacionalidade = inglesa.
+dinamarques tem nacionalidade = dinamarquesa.
+
+alemao tem casa = verde.
+noruegues tem casa = amarela.
+sueco tem casa = branca.
+ingles tem casa = vermelha.
+dinamarques tem casa = azul.
+
+alemao tem animal = peixes.
+noruegues tem animal = gatos.
+sueco tem animal = cachorros.
+ingles tem animal = passaros.
+dinamarques tem animal = cavalos.
+
+alemao tem cigarro = prince.
+noruegues tem cigarro = dunhill.
+sueco tem cigarro = bluemaster.
+ingles tem cigarro = pall mall.
+dinamarques tem cigarro = blends.
+
+alemao tem bebida = cafe.
+noruegues tem bebida = agua.
+sueco tem bebida = cerveja.
+ingles tem bebida = leite.
+dinamarques tem bebida = cha.
+
+alemao tem vizinhoEsq = ingles.
+noruegues tem vizinhoEsq = nil.
+sueco tem vizinhoEsq = alemao.
+ingles tem vizinhoEsq = dinamarques.
+dinamarques tem vizinhoEsq = noruegues.
+
+alemao tem vizinhoDir = sueco.
+noruegues tem vizinhoDir = dinamarques.
+sueco tem vizinhoDir = nil.
+ingles tem vizinhoDir = alemao.
+dinamarques tem vizinhoDir = ingles.
+
