1a7f1053aac5f7906f87ff8cc5379cbe51ab053d
Matheus Leite <leitematheus7@hotmail.com>
2014-10-10 15:32:58 -0300

    Adding the necessary tips for the challenge.    To see the tips, type: dicas.

--- DesafioEinstein.pl
+++ DesafioEinstein.pl
@@ -44,23 +44,40 @@ sueco tem bebida = cerveja.
 ingles tem bebida = leite.
 dinamarques tem bebida = cha.
 
-alemao temUm vizinhoEsq = ingles.
-noruegues temUm vizinhoEsq = nil.
-sueco temUm vizinhoEsq = alemao.
-ingles temUm vizinhoEsq = dinamarques.
-dinamarques temUm vizinhoEsq = noruegues.
+alemao tem vizinhoEsq = ingles.
+noruegues tem vizinhoEsq = nil.
+sueco tem vizinhoEsq = alemao.
+ingles tem vizinhoEsq = dinamarques.
+dinamarques tem vizinhoEsq = noruegues.
 
-alemao temUm vizinhoDir = sueco.
-noruegues temUm vizinhoDir = dinamarques.
-sueco temUm vizinhoDir = nil.
-ingles temUm vizinhoDir = alemao.
-dinamarques temUm vizinhoDir = ingles.
+alemao tem vizinhoDir = sueco.
+noruegues tem vizinhoDir = dinamarques.
+sueco tem vizinhoDir = nil.
+ingles tem vizinhoDir = alemao.
+dinamarques tem vizinhoDir = ingles.
 
 regras:-write('############# - REGRAS - #############'),nl,
 		write('1- Existem 5 casas de diferentes cores'),nl,
 	    write('2- Em cada casa mora uma pessoa de uma diferente nacionalidade'),nl,
 		write('3- Esses 5 proprietarios bebem diferentes bebidas, fumam diferentes tipos de cigarros e tem um certo animal de estimacao'),nl,
 		write('4- Nenhum deles tem o mesmo animal, fumam o mesmo cigarro ou bebem a mesma bebida.'),nl.
+
+dicas:-write('############# - DICAS - #############'),nl,
+	   write(' A - O Noruegues vive na primeira casa.'),nl,
+	   write(' B - O Ingles vive na casa Vermelha.'),nl,
+	   write(' C - O Sueco tem Cachorros como animais de estimacao.'),nl,
+	   write(' D - O Dinamarques bebe Cha.'),nl,
+       write(' E - A casa Verde fica do lado esquerdo da casa Branca.'),nl,
+	   write(' F - O homem que vive na casa Verde bebe Cafe.'),nl,
+	   write(' G - O homem que fuma Pall Mall cria Passaros.'),nl,
+	   write(' H - O homem que vive na casa Amarela fuma Dunhill.'),nl,
+       write(' I - O homem que vive na casa do meio bebe Leite.'),nl,
+       write(' J - O homem que fuma Blends vive ao lado do que tem Gatos.'),nl,
+	   write(' K - O homem que cria Cavalos vive ao lado do que fuma Dunhill.'),nl,
+	   write(' L - O homem que fuma BlueMaster bebe Cerveja.'),nl,
+	   write(' M - O Alemao fuma Prince.'),nl,
+	   write(' N - O Noruegues vive ao lado da casa Azul.'),nl,
+ 	   write(' O - O homem que fuma Blends eh vizinho do que bebe Agua.'),nl.
 
 
 
