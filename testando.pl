a28bde8f311c0940f5ef1891e1a5c8651b4bc23c
Makrau <iago006@hotmail.com>
2014-10-12 22:17:47 -0300

    add wrong guess message.

--- testando.pl
+++ testando.pl
@@ -15,16 +15,16 @@ menu:-  repeat,
 	X==0,
 	!.
 
-palpite(0,_):- !.
+palpite(0,0):- !.
 
-palpite('casa1','noruegues'):- assert(acertou(casa1,noruegues)).
-palpite('casa1','amarela'):- assert(acertou(casa1,amarela)).
+palpite('casa1','noruegues'):- assert(acertou(casa1,noruegues)),!.
+palpite('casa1','amarela'):- assert(acertou(casa1,amarela)),!.
 palpite('casa1','agua'):- assert(acertou(casa1,agua)).
-palpite('casa1','dunhill'):- assert(acertou(casa1,dunhill)).
-palpite('casa1','gatos'):- assert(acertou(casa1,gatos)).
-palpite('casa2','azul'):- assert(acertou(casa2,azul)).
-palpite('casa2','dinamarques'):- assert(acertou(casa2,dinamarques)).
-palpite('casa2','cha'):- assert(acertou(casa2,cha)).
+palpite('casa1','dunhill'):- assert(acertou(casa1,dunhill)),!.
+palpite('casa1','gatos'):- assert(acertou(casa1,gatos)),!.
+palpite('casa2','azul'):- assert(acertou(casa2,azul)),!.
+palpite('casa2','dinamarques'):- assert(acertou(casa2,dinamarques)),!.
+palpite('casa2','cha'):- assert(acertou(casa2,cha)),!.
 palpite('casa2','blends'):- assert(acertou(casa2,blends)).
 palpite('casa2','cavalos'):- assert(acertou(casa2,cavalos)).
 palpite('casa3','vermelho'):- assert(acertou(casa3,vermelho)).
@@ -42,4 +42,4 @@ palpite('casa5','sueco'):- assert(acertou(casa5,sueco)).
 palpite('casa5','cerveja'):- assert(acertou(casa5,cerveja)).
 palpite('casa5','bluemaster'):- assert(acertou(casa5,bluemaster)).
 palpite('casa5','cachorros'):- assert(acertou(casa5,cachorros)).
-palpite.
+palpite(_,_):- write('Palpite errado!'), nl.
